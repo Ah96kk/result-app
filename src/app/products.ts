43957aecdb7158ed4d3a408a0aa9e8749affd6a2
kkkff95 <planet.kkfff95@gmail.com>
2023-01-25 20:15:13 +0900

    Anguralフロントエンド作成

---
+++ src/app/products.ts
@@ -0,0 +1,51 @@
+export const products = [
+    {
+      coverImage: './assets/img/phone-cover.jpg',
+      name: 'Phone XL',
+      price: 799,
+      description: 'A large phone with one of the best screens',
+      heading1: 'サンプルテキスト',
+      heading2: 'サンプルテキスト',
+      heading3: 'サンプルテキスト',
+      headingtext1: ' サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト ',
+      headingtext2: 'テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章',
+      headingtext3: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.'
+    },
+    {
+      name: 'Phone Mini',
+      coverImage: './assets/img/phone-cover.jpg',
+      price: 699,
+      description: 'A great phone with one of the best cameras',
+      heading1: 'サンプルテキスト',
+      heading2: 'サンプルテキスト',
+      heading3: 'サンプルテキスト',
+      headingtext1: ' サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト ',
+      headingtext2: 'テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章',
+      headingtext3: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.'
+    },
+    {
+      name: 'Phone Standard',
+      coverImage: './assets/img/phone-cover.jpg',
+      heading1: 'サンプルテキスト',
+      heading2: 'サンプルテキスト',
+      heading3: 'サンプルテキスト',
+      price: 299,
+      description: '',
+      headingtext1: ' サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト ',
+      headingtext2: 'テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章',
+      headingtext3: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.'
+    },
+    {
+      name: 'Phone SE',
+      coverImage: './assets/img/phone-cover.jpg',
+      heading1: 'サンプルテキスト',
+      heading2: 'サンプルテキスト',
+      heading3: 'サンプルテキスト',
+      price: 199,
+      description: '',
+      headingtext1: ' サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト ',
+      headingtext2: 'テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章',
+      headingtext3: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.'
+    }
+      
+  ];
